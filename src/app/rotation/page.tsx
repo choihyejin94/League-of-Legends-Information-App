35bb2b0ab51ef9809f1e73434f4642e150749f1a
최혜진 <choihyejin94@naver.com>
2024-12-18 13:00:23 +0900

    배포 후 로테이션 오류 수정

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -33,32 +33,38 @@ const RotationPage = () => {
       <h1 className="text-4xl font-extrabold text-white text-center mb-10 bg-gradient-to-r from-red-500 to-red-700 p-4 rounded-lg shadow-lg">
         챔피언 로테이션 (이번주 무료로 플레이 할 수 있어요!)
       </h1>
-      <ul className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-10">
-        {rotation.map((champId) => {
-          const matchedChamp = Object.values(champion).find(
-            (champ) => champ.key === champId.toString()
-          );
-          return matchedChamp ? (
-            <li
-              key={matchedChamp.key}
-              className="border border-white rounded-lg overflow-hidden"
-            >
-              <div className="flex justify-center items-center p-4">
-                <Image
-                  src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${matchedChamp.id}_0.jpg`}
-                  alt={matchedChamp.name}
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <h3 className="text-xl font-bold text-red-600 text-center m-2">
-                {matchedChamp.name}
-              </h3>
-              <p className="text-center mb-4">{matchedChamp.title}</p>
-            </li>
-          ) : null;
-        })}
-      </ul>
+      {rotation.length > 0 ? (
+        <ul className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-10">
+          {rotation.map((champId) => {
+            const matchedChamp = Object.values(champion).find(
+              (champ) => champ.key === champId.toString()
+            );
+            return matchedChamp ? (
+              <li
+                key={matchedChamp.key}
+                className="border border-white rounded-lg overflow-hidden"
+              >
+                <div className="flex justify-center items-center p-4">
+                  <Image
+                    src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${matchedChamp.id}_0.jpg`}
+                    alt={matchedChamp.name}
+                    width={200}
+                    height={200}
+                  />
+                </div>
+                <h3 className="text-xl font-bold text-red-600 text-center m-2">
+                  {matchedChamp.name}
+                </h3>
+                <p className="text-center mb-4">{matchedChamp.title}</p>
+              </li>
+            ) : null;
+          })}
+        </ul>
+      ) : (
+        <p className="text-center text-white">
+          챔피언 로테이션 정보를 불러오는 중입니다...
+        </p>
+      )}
     </div>
   );
 };
